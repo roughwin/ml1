d7992e7a5fef53320a2bfeba8d598fdaeff9e479
roughwin <roughwin@qq.com>
2018-11-04 19:22:11 +0800

    SGD classifier

--- sci/x.py
+++ sci/x.py
@@ -25,10 +25,38 @@ colors = ['red', 'greenyellow', 'blue']
 for i in range(len(colors)):
   xs = Xtrain[:, 0][Ytrain == i]
   ys = Xtrain[:, 1][Ytrain == i]
-  plt.scatter(xs, ys, c=colors[i])
 
+  # plt.scatter(xs, ys, c=colors[i])
+
+
+
+
+
+## SGD Classifier
+
+from sklearn.linear_model import SGDClassifier
+
+clf = SGDClassifier()
+clf.fit(Xtrain, Ytrain)
+
+print('clf.coef', clf.coef_)
+print('clf.intercept', clf.intercept_)
+
+Xmin, Xmax = Xtrain[:, 0].min() - 0.5, Xtrain[:, 0].max() + 0.5
+Ymin, Ymax = Xtrain[:, 1].min() - 0.5, Xtrain[:, 1].max() - 0.5
+xs = np.arange(Xmin, Xmax, 0.5)
+
+fig, axes = plt.subplots(1,3)
+fig.set_size_inches(10, 6)
+for i in [0, 1, 2]:
+  axes[i].set_aspect('equal')
+  axes[i].set_xlim(Xmin, Xmax)
+  axes[i].set_ylim(Ymin, Ymax)
+  xx = Xtrain[:, 0]
+  yy = Xtrain[:, 1]
+  print(axes[i])
+  axes[i].scatter(xx, yy, c=Ytrain, cmap=plt.cm.prism)
+  ys = (-clf.intercept_[i] - xs * clf.coef_[i, 0])/ clf.coef_[i, 1]
+  axes[i].plot(xs, ys)
 
 plt.show()
-
-# print(x.shape, y.shape)
-# print(x[0], y[0])
